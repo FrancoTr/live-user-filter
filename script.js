5e967f962853926fbaf581fccfd604968b0891f1
Franco Traverso <franco-t20@outlook.com>
2021-08-30 22:31:03 -0300

    call getData and receive the fetched json from randomuser

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const result = document.getElementById("result");
 const filter = document.getElementById("filter");
 const listItems = [];
+getData();
 
 async function getData() {
   const res = await fetch("https://randomuser.me/api?results=15"); //brings 15 random users
+
+  const data = await res.json();
+  console.log(data);
 }
