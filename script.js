a695b2056a0d42a0cf088b201f2ad356d7c34311
Franco Traverso <franco-t20@outlook.com>
2021-08-30 22:34:29 -0300

    change query parameter to bring 50 results instead of 15

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const listItems = [];
 getData();
 
 async function getData() {
-  const res = await fetch("https://randomuser.me/api?results=15"); //brings 15 random users
+  const res = await fetch("https://randomuser.me/api?results=50"); //brings 50 random users
 
   const data = await res.json();
   console.log(data);
