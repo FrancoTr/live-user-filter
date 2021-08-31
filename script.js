8a6d21b0cfaab782c811bf16d553e91f252dc9df
Franco Traverso <franco-t20@outlook.com>
2021-08-30 22:45:07 -0300

    destructure data and add each user as an HTML list item

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ getData();
 async function getData() {
   const res = await fetch("https://randomuser.me/api?results=50"); //brings 50 random users
 
-  const data = await res.json();
-  console.log(data);
+  const { results } = await res.json();
+
+  // Clear results
+  results.innerHTML = "";
+
+  results.forEach((user) => {
+    const li = document.createElement("li"); //we create a list item for each of these users
+    listItems.push(li);
+    li.innerHTML = `
+        
+    `;
+  });
 }
